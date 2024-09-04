3f0b5f6833b1b526d651deb1a14fad7e1fff76bf
Thales Gomes <thaleshng@gmail.com>
2024-09-04 09:04:37 -0300

    Atualizando questão do processo seletivo da Target

--- questao-3/questao-3.js
+++ questao-3/questao-3.js
@@ -1,20 +1,22 @@
 const fs = require('fs');
 
 function processFaturamento() {
-    let data = JSON.parse(fs.readFileSync('faturamento.json', 'utf-8'));
+    let data = JSON.parse(fs.readFileSync('dados.json', 'utf-8'));
 
     let faturamentos = data.filter(dia => dia.valor > 0).map(dia => dia.valor);
 
     let menorFaturamento = Math.min(...faturamentos);
     let maiorFaturamento = Math.max(...faturamentos);
 
-    let somaFaturamento = faturamentos.reduce((acc, valor) => acc + valor, 0); // 2200
-    let mediaFaturamento = somaFaturamento / faturamentos.length; // 314.28    
-
+    let somaFaturamento = faturamentos.reduce((acc, valor) => acc + valor, 0);
+    
+    let mediaFaturamento = somaFaturamento / faturamentos.length; 
+    
     let diasAcimaDaMedia = faturamentos.filter(valor => valor > mediaFaturamento).length;
 
     console.log(`Menor faturamento: R$ ${menorFaturamento}`);
     console.log(`Maior faturamento: R$ ${maiorFaturamento}`);
+    console.log(`Média de faturamento: R$ ${mediaFaturamento.toFixed(2)}`);
     console.log(`Número de dias com faturamento acima da média: ${diasAcimaDaMedia}`);
 }
 
